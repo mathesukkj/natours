42d529cd49525abbf1c89f15c28b7a0b4b870fd9
mathesukkj <kemuel.g7363@gmail.com>
2023-04-23 12:48:30 -0300

    refactor: basic code cleaning using router

--- app.js
+++ app.js
@@ -6,102 +6,79 @@ app.use(express.json());
 
 const tours = JSON.parse(readFileSync("./dev-data/data/tours-simple.json"));
 
-app.get("/", (req, res) => {
+const getAllTours = (req, res) => {
     res.status(200).send({
-        msg: "hi",
-        app: "natours",
+        tours,
     });
-});
+};
 
-app.get("/api/v1/tours", (req, res) => {
-    res.status(200).send({
-        status: "success",
-        results: tours.length,
-        data: {
-            tours,
-        },
-    });
-});
-
-app.get("/api/v1/tours/:id", (req, res) => {
-    const { id } = req.params;
-    const foundTour = tours.find((item) => item.id == id);
-
-    if (!foundTour) {
-        return res.status(404).send({
-            status: "failed",
-            message: "Invalid id",
-        });
-    }
-
-    res.status(200).send({
-        status: "success",
-        data: {
-            tour: foundTour,
-        },
-    });
-});
-
-app.patch("/api/v1/tours/:id", (req, res) => {
-    const { id } = req.params;
-    const foundTour = tours.find((item) => item.id == id);
-
-    if (!foundTour) {
-        return res.status(404).send({
-            status: "failed",
-            message: "Invalid id",
-        });
-    }
-
-    const updatedTour = { ...foundTour, ...req.body };
-    const updatedTours = tours.map((tour) => (tour.id === updatedTour.id ? updatedTour : tour));
-
-    writeFile("./dev-data/data/tours-simple.json", JSON.stringify(updatedTours), () => {
-        res.status(200).json({
-            status: "success",
-            data: {
-                tour: updatedTour,
-            },
-        });
-    });
-});
-
-app.post("/api/v1/tours", (req, res) => {
+const addNewTour = (req, res) => {
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({ id: newId }, req.body);
 
     tours.push(newTour);
     writeFile("./dev-data/data/tours-simple.json", JSON.stringify(tours), () => {
         res.status(201).send({
-            status: "success",
-            data: {
-                tour: newTour,
-            },
+            tour: newTour,
         });
     });
-});
+};
 
-app.delete("/api/v1/tours/:id", (req, res) => {
+const getTourById = (req, res) => {
     const { id } = req.params;
     const foundTour = tours.find((item) => item.id == id);
 
     if (!foundTour) {
         return res.status(404).send({
-            status: "failed",
             message: "Invalid id",
         });
     }
 
-    const updatedTours = tours
-        .map((tour) => {
-            return tour.id == id ? null : tour;
-        })
-        .filter((tour) => tour != null);
+    res.status(200).send({
+        tour: foundTour,
+    });
+};
+
+const updateTour = (req, res) => {
+    const { id } = req.params;
+    const foundTour = tours.find((item) => item.id == id);
+
+    if (!foundTour) {
+        return res.status(404).send({
+            message: "Invalid id",
+        });
+    }
+
+    const updatedTour = { ...foundTour, ...req.body };
+    const updatedTours = tours.map((tour) => (tour.id == id ? updatedTour : tour));
+
+    writeFile("./dev-data/data/tours-simple.json", JSON.stringify(updatedTours), () => {
+        res.status(200).send({
+            tour: updatedTour,
+        });
+    });
+};
+
+const deleteTour = (req, res) => {
+    const { id } = req.params;
+    const foundTour = tours.find((item) => item.id == id);
+
+    if (!foundTour) {
+        return res.status(404).send({
+            message: "Invalid id",
+        });
+    }
+
+    const updatedTours = tours.filter((tour) => tour.id != id);
 
     writeFile("./dev-data/data/tours-simple.json", JSON.stringify(updatedTours), () => {
         res.status(204).send();
     });
-});
+};
+
+app.route("/api/v1/tours").get(getAllTours).post(addNewTour);
+
+app.route("/api/v1/tours/:id").get(getTourById).patch(updateTour).delete(deleteTour);
 
 app.listen(8000, () => {
     console.log("Listening on port 8000");
