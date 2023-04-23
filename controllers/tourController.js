277c1c9b6321a928a549e7d5dec0c4af7f1a02e5
mathesukkj <kemuel.g7363@gmail.com>
2023-04-23 14:16:08 -0300

    refactor: add controllers for each route

---
+++ controllers/tourController.js
@@ -0,0 +1,73 @@
+import { readFileSync, writeFile } from "fs";
+
+const tours = JSON.parse(readFileSync("./dev-data/data/tours-simple.json"));
+
+export const getAllTours = (req, res) => {
+    res.status(200).send({
+        tours,
+    });
+};
+
+export const addNewTour = (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+    writeFile("./dev-data/data/tours-simple.json", JSON.stringify(tours), () => {
+        res.status(201).send({
+            tour: newTour,
+        });
+    });
+};
+
+export const getTourById = (req, res) => {
+    const { id } = req.params;
+    const foundTour = tours.find((item) => item.id == id);
+
+    if (!foundTour) {
+        return res.status(404).send({
+            message: "Invalid id",
+        });
+    }
+
+    res.status(200).send({
+        tour: foundTour,
+    });
+};
+
+export const updateTour = (req, res) => {
+    const { id } = req.params;
+    const foundTour = tours.find((item) => item.id == id);
+
+    if (!foundTour) {
+        return res.status(404).send({
+            message: "Invalid id",
+        });
+    }
+
+    const updatedTour = { ...foundTour, ...req.body };
+    const updatedTours = tours.map((tour) => (tour.id == id ? updatedTour : tour));
+
+    writeFile("./dev-data/data/tours-simple.json", JSON.stringify(updatedTours), () => {
+        res.status(200).send({
+            tour: updatedTour,
+        });
+    });
+};
+
+export const deleteTour = (req, res) => {
+    const { id } = req.params;
+    const foundTour = tours.find((item) => item.id == id);
+
+    if (!foundTour) {
+        return res.status(404).send({
+            message: "Invalid id",
+        });
+    }
+
+    const updatedTours = tours.filter((tour) => tour.id != id);
+
+    writeFile("./dev-data/data/tours-simple.json", JSON.stringify(updatedTours), () => {
+        res.status(204).send();
+    });
+};
