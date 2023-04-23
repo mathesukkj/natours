0d31144d6b204847432cc6172142c1216e00522c
mathesukkj <kemuel.g7363@gmail.com>
2023-04-23 14:50:55 -0300

    feat: add checkBody function

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,12 +6,13 @@ import {
     updateTour,
     deleteTour,
     checkId,
+    checkBody,
 } from "../controllers/tourController.js";
 
 export const router = express.Router();
 
 router.param("id", checkId);
 
-router.route("/").get(getAllTours).post(addNewTour);
+router.route("/").get(getAllTours).post(checkBody, addNewTour);
 
 router.route("/:id").get(getTourById).patch(updateTour).delete(deleteTour);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,12 +6,13 @@ import {
     updateUser,
     deleteUser,
     checkId,
+    checkBody,
 } from "../controllers/userController.js";
 
 export const router = express.Router();
 
 router.param("id", checkId);
 
-router.route("/").get(getAllUsers).post(addNewUser);
+router.route("/").get(getAllUsers).post(checkBody, addNewUser);
 
 router.route("/:id").get(getUserById).patch(updateUser).delete(deleteUser);
