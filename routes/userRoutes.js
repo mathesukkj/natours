35b23f48f31f3df080183254c2f949d7047df3bd
mathesukkj <kemuel.g7363@gmail.com>
2023-04-23 14:07:59 -0300

    refactor: modulate code into diff routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,82 @@
+import express from "express";
+import { readFileSync, writeFile } from "fs";
+
+const users = JSON.parse(readFileSync("./dev-data/data/users.json"));
+
+export const router = express.Router();
+
+const getAllUsers = (req, res) => {
+    res.status(200).send({
+        users,
+    });
+};
+
+const addNewUser = (req, res) => {
+    const lastId = users[users.length - 1]._id.split("6fa");
+
+    const newId = lastId[0] + (+lastId[1] + 1);
+    const newUser = Object.assign({ _id: newId, active: true, role: "user" }, req.body);
+
+    users.push(newUser);
+    writeFile("./dev-data/data/users.json", JSON.stringify(users), () => {
+        res.status(201).send({
+            user: newUser,
+        });
+    });
+};
+
+const getUserById = (req, res) => {
+    const { id } = req.params;
+    const foundUser = users.find((item) => item._id == id);
+
+    if (!foundUser) {
+        return res.status(404).send({
+            message: "Invalid id",
+        });
+    }
+
+    res.status(200).send({
+        user: foundUser,
+    });
+};
+
+const updateUser = (req, res) => {
+    const { id } = req.params;
+    const foundUser = users.find((user) => user._id == id);
+
+    if (!foundUser) {
+        return res.status(404).send({
+            message: "Invalid id",
+        });
+    }
+
+    const updatedUser = { ...foundUser, ...req.body };
+    const updatedUsers = users.map((user) => (user._id == id ? updatedUser : user));
+
+    writeFile("./dev-data/data/users.json", JSON.stringify(updatedUsers), () => {
+        res.status(200).send({
+            user: updatedUser,
+        });
+    });
+};
+
+const deleteUser = (req, res) => {
+    const { id } = req.params;
+    const foundUser = users.find((user) => user._id == id);
+
+    if (!foundUser) {
+        return res.status(404).send({
+            message: "Invalid id",
+        });
+    }
+
+    const updatedUsers = users.filter((user) => user._id != id);
+
+    writeFile("./dev-data/data/users.json", JSON.stringify(updatedUsers), () => {
+        res.status(204).send();
+    });
+};
+
+router.route("/").get(getAllUsers).post(addNewUser);
+
+router.route("/:id").get(getUserById).patch(updateUser).delete(deleteUser);
