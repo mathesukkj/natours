1eecca5a7a0633999367908209a14764d476f2ca
mathesukkj <kemuel.g7363@gmail.com>
2023-04-23 11:30:38 -0300

    feat: add post route, for adding new tours

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { readFileSync } from "fs";
+import { readFileSync, writeFile } from "fs";
 
 const app = express();
+app.use(express.json());
+
 const tours = JSON.parse(readFileSync("./dev-data/data/tours-simple.json"));
 
 app.get("/", (req, res) => {
@@ -18,6 +20,21 @@ app.get("/api/v1/tours", (req, res) => {
         data: {
             tours,
         },
+    });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+    writeFile("./dev-data/data/tours-simple.json", JSON.stringify(tours), () => {
+        res.status(201).json({
+            status: "success",
+            data: {
+                tour: newTour,
+            },
+        });
     });
 });
 
