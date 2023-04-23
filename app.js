b32d84c9dbeedc09a19c5df8f8431c28e87ccf98
mathesukkj <kemuel.g7363@gmail.com>
2023-04-23 11:39:59 -0300

    feat: add get tour by id route

--- app.js
+++ app.js
@@ -23,6 +23,24 @@ app.get("/api/v1/tours", (req, res) => {
     });
 });
 
+app.get("/api/v1/tours/:id", (req, res) => {
+    const { id } = req.params;
+    if (id > tours.length || id < 1 || isNaN(id)) {
+        return res.status(404).send({
+            status: "failed",
+            message: "Invalid id",
+        });
+    }
+
+    const foundTour = tours.find((item) => item.id == id);
+    res.status(200).send({
+        status: "success",
+        data: {
+            tour: foundTour,
+        },
+    });
+});
+
 app.post("/api/v1/tours", (req, res) => {
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({ id: newId }, req.body);
