ca75b45e581eaa5318f2e87b8965b2b7ab1533a0
mathesukkj <kemuel.g7363@gmail.com>
2023-04-23 11:15:58 -0300

    feat: add tours get route

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
+import { readFileSync } from "fs";
 
 const app = express();
+const tours = JSON.parse(readFileSync("./dev-data/data/tours-simple.json"));
 
 app.get("/", (req, res) => {
     res.status(200).json({
@@ -9,8 +11,14 @@ app.get("/", (req, res) => {
     });
 });
 
-app.post("/", (req, res) => {
-    res.send("post test");
+app.get("/api/v1/tours", (req, res) => {
+    res.status(200).send({
+        status: "success",
+        results: tours.length,
+        data: {
+            tours,
+        },
+    });
 });
 
 app.listen(8000, () => {
