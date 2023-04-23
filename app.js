647863db97327bfab9af11bfafd3ff2485b528d7
mathesukkj <kemuel.g7363@gmail.com>
2023-04-23 11:51:23 -0300

    feat: add placeholder update tour route

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 const tours = JSON.parse(readFileSync("./dev-data/data/tours-simple.json"));
 
 app.get("/", (req, res) => {
-    res.status(200).json({
+    res.status(200).send({
         msg: "hi",
         app: "natours",
     });
@@ -41,13 +41,30 @@ app.get("/api/v1/tours/:id", (req, res) => {
     });
 });
 
+app.patch("/api/v1/tours/:id", (req, res) => {
+    const { id } = req.params;
+    if (id > tours.length || id < 1 || isNaN(id)) {
+        return res.status(404).send({
+            status: "failed",
+            message: "Invalid id",
+        });
+    }
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            tour: "<UpdatedTour />",
+        },
+    });
+});
+
 app.post("/api/v1/tours", (req, res) => {
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({ id: newId }, req.body);
 
     tours.push(newTour);
     writeFile("./dev-data/data/tours-simple.json", JSON.stringify(tours), () => {
-        res.status(201).json({
+        res.status(201).send({
             status: "success",
             data: {
                 tour: newTour,
