aad169e94e700598040bfaca7f4f068621908a14
mathesukkj <kemuel.g7363@gmail.com>
2023-04-26 00:04:14 -0300

    refactor: start MVC refactoring

--- server.js
+++ server.js
@@ -8,36 +8,6 @@ const mongooseSettings = { useNewUrlParser: true, useCreateIndex: true, useFindA
 
 mongoose.connect(db, mongooseSettings).then(() => console.log("connected!"));
 
-const tourSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "A tour must have a name"],
-        unique: true,
-    },
-    rating: {
-        type: Number,
-        default: 4.5,
-    },
-    price: {
-        type: Number,
-        required: [true, "A tour must have a price"],
-    },
-});
-const Tour = mongoose.model("Tour", tourSchema);
-
-const testTour = new Tour({
-    name: "The Park Camper",
-    price: 376,
-});
-testTour
-    .save()
-    .then((data) => {
-        console.log(data);
-    })
-    .catch((e) => {
-        console.log("error when saving to db: " + e);
-    });
-
 app.listen(process.env.PORT, () => {
     console.log("Listening on port 8000");
 });
