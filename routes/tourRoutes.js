536b5badd09572652b722ee1908aadd87b827b11
mathesukkj <kemuel.g7363@gmail.com>
2023-04-29 11:32:20 -0300

    feat: add best and cheapest tours alias route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,10 +5,13 @@ import {
     getTourById,
     updateTour,
     deleteTour,
+    aliasBestTours,
 } from "../controllers/tourController.js";
 
 export const router = express.Router();
 
 router.route("/").get(getAllTours).post(addNewTour);
+
+router.route("/best-cheap").get(aliasBestTours, getAllTours);
 
 router.route("/:id").get(getTourById).patch(updateTour).delete(deleteTour);
