c6f55465fdffbd5c6d2ea7962e5a177eb5d268a3
mathesukkj <kemuel.g7363@gmail.com>
2023-04-30 18:41:09 -0300

    feat: add data unwinding and projecting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -118,3 +118,38 @@ export const getTourStats = async (req, res) => {
         });
     }
 };
+
+export const getMonthlyPlan = async (req, res) => {
+    try {
+        const year = +req.params.year;
+        const plan = await Tour.aggregate([
+            { $unwind: "$startDates" },
+            {
+                $match: {
+                    startDates: {
+                        $gte: new Date(`${year}-01-01`),
+                        $lte: new Date(`${year}-12-31`),
+                    },
+                },
+            },
+            {
+                $group: {
+                    _id: { $month: "$startDates" },
+                    qtyTourStarts: { $sum: 1 },
+                    tours: { $push: "$name" },
+                },
+            },
+            { $addFields: { month: "$_id" } },
+            { $project: { _id: 0 } },
+            { $sort: { qtyTourStarts: -1 } },
+        ]);
+
+        res.status(200).send({
+            plan,
+        });
+    } catch (err) {
+        res.status(404).send({
+            message: err,
+        });
+    }
+};
