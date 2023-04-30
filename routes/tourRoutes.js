ca8ebb634f7c5e7d240b894a6bcd4537bf9f1945
mathesukkj <kemuel.g7363@gmail.com>
2023-04-30 18:41:09 -0300

    feat: add data unwinding and projecting

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,6 +7,7 @@ import {
     deleteTour,
     aliasBestTours,
     getTourStats,
+    getMonthlyPlan,
 } from "../controllers/tourController.js";
 
 export const router = express.Router();
@@ -14,6 +15,8 @@ export const router = express.Router();
 router.route("/").get(getAllTours).post(addNewTour);
 
 router.route("/stats").get(getTourStats);
+
+router.route("/monthly-plan/:year").get(getMonthlyPlan);
 
 router.route("/best-cheap").get(aliasBestTours, getAllTours);
 
