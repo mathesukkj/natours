b3e92b13991dd0d4a917467df6dae90647f8faf6
mathesukkj <kemuel.g7363@gmail.com>
2023-05-02 22:27:34 -0300

    feat: add basic validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -7,6 +7,8 @@ const tourSchema = new mongoose.Schema(
             type: String,
             required: [true, "A tour must have a name"],
             unique: true,
+            maxlength: [40, "A tour name must have less than 40 characters"],
+            minlength: [10, "A tour name must have more than 10 characters"],
         },
         duration: {
             type: Number,
@@ -19,10 +21,16 @@ const tourSchema = new mongoose.Schema(
         difficulty: {
             type: String,
             required: [true, "A tour must have a difficulty"],
+            enum: {
+                values: ["easy", "medium", "difficult"],
+                message: "Difficulty must be either: easy, medium or difficult.",
+            },
         },
         ratingsAverage: {
             type: Number,
             default: 4.5,
+            min: [1, "Rating must be above 1.0"],
+            max: [1, "Rating must be below 5.0"],
         },
         ratingsQuantity: {
             type: Number,
