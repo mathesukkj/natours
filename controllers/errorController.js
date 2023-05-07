22f353d4395a3fa1c501e589aa605c518c6ca59b
mathesukkj <kemuel.g7363@gmail.com>
2023-05-07 11:14:32 -0300

    feat: add treatment for prod/dev errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,7 +1,41 @@
+import AppError from "../utils/appError.js";
+
+const handleCastError = (err) => {
+    const msg = `Invalid ${err.path}: ${err.value}`;
+    return new AppError(msg, 400);
+};
+
+const sendErrorDev = (err, res) => {
+    // if error happened in dev, show every info possible
+    res.status(err.statusCode).send({
+        message: err.message,
+        error: err,
+        stack: err.stack,
+    });
+};
+
+const sendErrorProd = (err, res) => {
+    // if error happened in prod, show as little info as possible
+    if (err.isOperational) {
+        res.status(err.statusCode).send({
+            message: err.message,
+        });
+    } else {
+        console.log("UNKNOWN ERROR: ", err);
+        res.status(500).send({
+            message: "Something went wrong!",
+        });
+    }
+};
+
 export default (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
 
-    res.status(err.statusCode).send({
-        message: err.message,
-    });
+    if (process.env.NODE_ENV === "development") {
+        sendErrorDev(err, res);
+    } else {
+        let error = { ...err };
+        if (error.kind == "ObjectId") error = handleCastError(error);
+        sendErrorProd(error, res);
+    }
 };
