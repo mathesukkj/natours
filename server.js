a5622e7678d735c890a00ac0f6df7b7799f56580
mathesukkj <kemuel.g7363@gmail.com>
2023-05-07 12:04:34 -0300

    feat: add error handling in main server

--- server.js
+++ server.js
@@ -6,8 +6,18 @@ import mongoose from "mongoose";
 const db = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
 const mongooseSettings = { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false };
 
-mongoose.connect(db, mongooseSettings).then(() => console.log("connected!"));
+mongoose
+    .connect(db, mongooseSettings)
+    .then(() => console.log("\x1b[92mSuccessfully connected to database!\x1b[0m"));
 
-app.listen(process.env.PORT, () => {
-    console.log("Listening on port 8000");
+const server = app.listen(process.env.PORT, () => {
+    console.log("\x1b[36mListening on port 8000\x1b[0m");
+});
+
+process.on("unhandledRejection", (err) => {
+    console.log("\x1b[91mUNHANDLED REJECTION!!");
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
 });
