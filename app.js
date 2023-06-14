e14d8995f03c8bc5817e42781800f833276fd40b
mathesukkj <kemuel.g7363@gmail.com>
2023-06-13 22:08:08 -0300

    feat: add rate limiting to api

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { router as tourRouter } from "./routes/tourRoutes.js";
 import { router as userRouter } from "./routes/userRoutes.js";
 import AppError from "./utils/appError.js";
 import globalErrorHandler from "./controllers/errorController.js";
+import { rateLimit } from "express-rate-limit";
 
 const app = express();
 
@@ -16,6 +17,14 @@ app.use((req, res, next) => {
 });
 
 app.use(express.static("public/"));
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100, // 100 reqs - 15 min
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+app.use("/api", limiter);
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
